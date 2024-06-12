cf942b214158265fb1f15b02f89e88f6689a70c9
eliezerschlickmann <54064483+eliezerschlickmann@users.noreply.github.com>
2024-06-12 07:41:08 -0300

    Delete api-node directory

--- api-node/model/Event.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const EventSchema = new mongoose.Schema({
-    type: {type: String, required: true},
-    timestamp: {type: Date, required: true},
-    additionalDetails: {type: String, required: false}
-});
-
-module.exports = mongoose.model('Event', EventSchema, 'event');
